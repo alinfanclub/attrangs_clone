28f18693c08232aa6a84911ba35783fe97e25737
e759ksh <e759ksh@naver.com>
2022-07-12 18:13:25 +0900

    아뜨랑스 셋업

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+        <div id="searchArea">
+            <div id="searchBar">
+                <form action="@">
+                    <input type="text" name="" id="">
+                    <button type="submit">
+                        검색
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div id="logoArea"></div>
+        <div id="userArea"></div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "HeaderView",
+}
+</script>
+
+<style>
+
+</style>
